0f5fc65729daca506d19d2aae5e5910ce00af81c
alramalho <alexandre.ramalho.1998@gmail.com>
2023-04-20 10:07:42 +0200

    add endpoint

--- wipe_table.py
+++ wipe_table.py
@@ -15,7 +15,7 @@ def delete_table_entries(table_name, aws_endpoint=None):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Delete all entries from DynamoDB table.')
     parser.add_argument('table_name', type=str, help='Name of the table to delete entries from.')
-    parser.add_argument('--aws_endpoint', type=str, help='AWS endpoint URL (optional).')
+    parser.add_argument('-e', '--aws_endpoint', type=str, help='AWS endpoint URL (optional).')
     args = parser.parse_args()
     delete_table_entries(args.table_name, args.aws_endpoint)
     print('Done ✅')
